5e88c22c4fdae173510394bd6257512e2bbdcdd2
Daniel-Kerr <dan.kerr@rvlti.com>
2017-06-22 08:48:53 -0700

    Small adjustment on regression.

--- tests/full_regression.sh
+++ tests/full_regression.sh
@@ -16,12 +16,12 @@ clear;
 # in case these do not exist
 touch $logdir/regression_summary.log;
 touch $logdir/failcount.log;
-touch $logdir/AssertionFailure.log
+touch $logdir/Assertion_Failure.log
 touch $logdir/error.log;
 #clear out any old logs
 cat /dev/null > $logdir/regression_summary.log;
 cat /dev/null > $logdir/failcount.log;
-cat /dev/null > $logdir/assertionfailure.log;
+cat /dev/null > $logdir/assertion_failure.log;
 cat /dev/null > $logdir/error.log;
 echo "-----------------------------------------------------" | tee -a $logdir/regression_summary.log;
 echo "Date: $today         $now                  Host:$host" | tee -a $logdir/regression_summary.log;
@@ -44,7 +44,7 @@ for testname in $listoftests
 		printf "End $testname\n" | tee -a $logdir/$testname.log;
 		
 		printf "_________________________________________________\n" | tee -a $logdir/regression_summary.log;
-		grep -H "AssertionFailure" $logdir/$testname.log | tee -a $logdir/assertionfailure.log;	
+		grep -H "AssertionFailure" $logdir/$testname.log | tee -a $logdir/assertion_failure.log;	
 		grep -H "Error" $logdir/$testname.log | tee -a $logdir/error.log;
 		printf "Results for $testname\n\n" | tee -a $logdir/regression_summary.log;
 		cat $logdir/$testname.log | grep -A 18 "executed" | grep -A 18 "failed" | tee -a $logdir/regression_summary.log;
@@ -55,7 +55,7 @@ for testname in $listoftests
 
 		#  this had a bug --> totalfailures=$(cat $logdir/failcount.log | awk '{ sum += $1 } END { print sum }');
 		totalfailures=$(cat $logdir/*.log | grep "test-scripts" | awk '{print substr($0,45,10)}'  | awk '{ sum += $1 } END { print sum }');
-		totalassertionfailures=$(cat $logdir/assertionfailure.log | wc -l | awk '{ sum += $1 } END { print sum }' );
+		totalassertionfailures=$(cat $logdir/assertion_failure.log | wc -l | awk '{ sum += $1 } END { print sum }' );
 		grep -H "Error" $logdir/*.log | tee -a $logdir/error.log;
 		totalerrors=$(wc -l $logdir/error.log);
 		totalpasses=$(cat $logdir/*.log | grep "test-scripts" | awk '{print substr($0,37,4)}'  | awk '{ sum += $1 } END { print sum }');
