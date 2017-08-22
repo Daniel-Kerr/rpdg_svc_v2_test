a5649789d6cf36eac30a822d05e14f1abc8088bc
DESKTOP-MUKL5RU\nickp <nick.preiser@gmail.com>
2017-08-22 10:01:12 -0700

    moved hw tests to there own folders,

--- tests/hardware_tests/LV/lv_board_test.sh
+++ tests/hardware_tests/LV/lv_board_test.sh
@@ -9,7 +9,9 @@ if ping -n 1 -w 500 $target > /dev/null; then
     echo "relay-01 OK"
 	echo "rewritting the environment file"
     echo '{"values":[{"key":"host","value":"'$target'"}]}' > ./environment.json
-    newman run -e ./environment.json LV_BoardTest_PLC_WDContact.postman_collection.json
+    newman run -e ./environment.json LV_BoardTest_PLC_WDContact.postman_collection.json --bail
+	
+	newman run -e ./environment.json LV_BoardTest_0_10_Input.postman_collection.json --bail
   else
     echo "relay-01 NOT FOUND, TEST STOPPED "
   fi
